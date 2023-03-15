6f62a236086806be552e84ff401d6dc3cd14bf1f
kiranchoudhary123 <kiranchoudhary0808@gmail.com>
2023-03-15 19:09:50 +0530

    changes bank details in sendmail

--- supertech/custom_script/sales_invoice/sales_invoice.py
+++ supertech/custom_script/sales_invoice/sales_invoice.py
@@ -1,18 +1,22 @@
 import frappe
 from frappe.utils import now_datetime, time_diff_in_hours,pretty_date, now, add_to_date
 from datetime import datetime
+from email.utils import formataddr
 
 def sendmail1(doc, method):
-        notification_email_id = frappe.db.get_value("Email Account", "Notifications", "email_id")
-        kiran_email_id = frappe.db.get_value("Email Account", "kiran choudhary gmail", "email_id")
+        supertech = frappe.db.get_value("Email Account", "Notifications", "email_id")
+        sender_name = "Supertech"
+        director = frappe.db.get_value("Email Account", "choudharykiran9721@gmail.com", "email_id")
+        # plant_manager = "gaurang@supertechfabrics.com"
+        plant_manager = "mrinal.a@indictranstech.com "
         ms = f'''To<br>
 {doc.customer_name}<br><br>
 Dear Sir,<br>
 Please find below the dispatch particulars against your order no:  {doc.po_no}
 <br><br>
 Invoice No : {doc.name}<br>
-Invoice Date : {doc.posting_date}<br>
-Invoice Amount : {doc.grand_total}<br>
+Invoice Date : { frappe.utils.formatdate(doc.posting_date, "dd-mm-yyyy") }<br>
+Invoice Amount : {doc.get_formatted("grand_total") }<br>
 
 Transporter Name : {doc.transporter_name}<br>
 L R No : {doc.lr_number} <br>
@@ -29,13 +33,14 @@ Supertech Fabrics<br><br>
 
 '''
         
-        all_cc = [ kiran_email_id, doc.i_poc_email, doc.account_head_email]
+        all_cc = [ director, plant_manager, doc.i_poc_email, doc.account_head_email]
+        sender = formataddr((sender_name, supertech))
         frappe.sendmail(
         recipients =doc.contact_email,
         subject = "Dispatch details are here",
         message = ms,
         cc = all_cc,
-        sender = notification_email_id,
+        sender = sender,
         reference_doctype = "Sales Invoice",
         reference_name	= doc.name,
         now =  True,
