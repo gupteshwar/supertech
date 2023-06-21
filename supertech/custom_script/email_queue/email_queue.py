406412b36e22910e4654504562151057d8bd036a
kiranchoudhary123 <kiranchoudhary0808@gmail.com>
2023-06-21 18:22:11 +0530

    added time condition in email queue function

--- supertech/custom_script/email_queue/email_queue.py
+++ supertech/custom_script/email_queue/email_queue.py
@@ -1,4 +1,7 @@
 import frappe
+from frappe.utils import now_datetime, time_diff_in_hours,pretty_date, now, add_to_date
+from datetime import datetime, date
+from email.utils import formataddr
 
 def delete_email_queues():
     # '''method deletes email queues whose status is in Sent and/or Error'''
@@ -6,7 +9,11 @@ def delete_email_queues():
 
     #checking the day of the week, the script only works on monday, wednesday and friday
     # if not frappe.utils.get_datetime().weekday() in [1,3,5,6]:
-    if frappe.utils.get_datetime().weekday() in [0,2,4]:
+    c = str(now_datetime())[11:13]
+    d = int(c)
+    a = "01"
+    b = int(a)
+    if frappe.utils.get_datetime().weekday() in [0,1,2,3,4,5,6] and d == b:
 
         print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
         #getting the tuple of names of Email Queues with status in Sent or Error
