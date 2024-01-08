eef746098ea4a024f600905dc3ef03151c3c6b79
kiranchoudhary123 <kiranchoudhary0808@gmail.com>
2024-01-08 16:14:48 +0530

    changes in sales order before save validation

--- supertech/custom_script/sales_order/sales_order.py
+++ supertech/custom_script/sales_order/sales_order.py
@@ -3,6 +3,6 @@ from frappe import _
 
 
 def before_save(doc, method):
-    if doc.customer_category == "Export" and doc.taxes_and_charges != None:
+    if doc.customer_category == "Export" and doc.taxes_and_charges != '':
         frappe.throw(frappe._("You can not select sales taxes and template,if customer category is 'Export'."))
      
